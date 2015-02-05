8fc64059d942f22d4d5e29070fec6587a1b25bd8
david-borchers <dlb@st-andrews.ac.uk>
2015-02-05 15:39:51 +0000

    Added ability to have a near y distance group (to deal with rounding to zero) instead of using exact distances for small forward distances. Main change was to negllik.xandy. Also changed negllik.xy, fit.xy, fit.hmltm, and est.hmltm (just added argument groupfromy to these).

--- R/hsltm.R
+++ R/hsltm.R
@@ -38,7 +38,8 @@ gety.obs <- function(Ix, Inull_yobs, Iyobs, Itheta_f, Itheta_b, Iymax, Idy) {
 #' @param Itheta_b REDUNDANT; must = 90
 #' @param Ially Flag for whether or not to return probabilities at all forward distances in view.
 #' @param Icdf Flag for whether or not to return cumulative distribution function in forward dimension.
-#' 
+#' This differes from specifying Ially=TRUE in that Ially=TRUE calculates the cdf from ymax 
+#' to y=0, whereas Icdf=TRUE calculates the cdf from ymax to y.
 p.xy1 <- function(Ix, Iy, Ihfun, Ib, Ipcu, IPi, Idelta, Iymax, Idy, Itheta_f, Itheta_b, Ially, Icdf){
   .Call( "bias_p_xy1", Ix, Iy, Ihfun, Ib, Ipcu, IPi, Idelta, Iymax, Idy, Itheta_f, Itheta_b, Ially, Icdf, PACKAGE = "hsltm" )
 }
@@ -1274,17 +1275,21 @@ fxfit.plot=function(hmltm,allx=FALSE,values=TRUE,breaks=NULL,ylim=NULL,xlab=NULL
 #' @param nx NOT SURE - I think it is redundant - CHECK
 #' @param hessian Logical; if TRUE Hessian matrix is returned
 #' @param control list controlling \code{\link{optim}} optimisation. 
+#' @param groupfromy a forward distance (y) below which all y's are grouped into a single
+#' interval in the likelihood function (i.e. exact y,s < groupfromy are combined into
+#' an interval rather than passed as exact distances).
 #'
 #' @return A list comprising \code{\link{optim}} ouptut plus element $par containing the estimated parameters 
 #'  on the same scale as input parameters pars (which are transformed before calling \code{\link{optim}}).
 fit.xy=function(pars,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta=delta,
                 theta.f=0,theta.b=90,W,ymax,dy,nx=50,hessian=FALSE,
-                control=list(trace=5,reltol=1e-6,maxit=200))
+                control=list(trace=5,reltol=1e-6,maxit=200),groupfromy=NULL)
 {
   b=tfm(pars,FUN)
   fit=optim(par=b,fn=negllik.xy,control=control,hessian=hessian,xy=xy,
             FUN=FUN,models=models,pm=pm,Pi=Pi,delta=delta,
-            theta.f=theta.f,theta.b=theta.b,W=W,ymax=ymax,dy=dy,nx=nx)
+            theta.f=theta.f,theta.b=theta.b,W=W,ymax=ymax,dy=dy,nx=nx,
+            groupfromy=groupfromy)
   fit$par=invtfm(fit$par,FUN)
   return(fit)
 }
@@ -1309,6 +1314,9 @@ fit.xy=function(pars,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta=delta,
 #'              \code{$nx (scalar)} - determines the number of intervals to use with Simpson's rule 
 #'              integration over y. \code{nx=64} seems safe; smaller number makes computing faster.
 #' @param control.optim as required by \code{\link{optim}}.
+#' @param groupfromy a forward distance (y) below which all y's are grouped into a single
+#' interval in the likelihood function (i.e. exact y,s < groupfromy are combined into
+#' an interval rather than passed as exact distances).
 #'
 #' @return A list comprising:
 #' \itemize{
@@ -1330,9 +1338,10 @@ fit.xy=function(pars,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta=delta,
 #'  
 #' @references Borchers, D.L., Zucchini, W., Heide-Jorgenssen, M.P., Canadas, A. and Langrock, R. 
 #' 2013. Using hidden Markov models to deal with availability bias on line transect surveys. Biometrics.
-fit.hmltm=function(xy,pars,FUN,models=list(y=NULL,x=NULL),survey.pars,hmm.pars,control.fit,control.optim)
+fit.hmltm=function(xy,pars,FUN,models=list(y=NULL,x=NULL),survey.pars,hmm.pars,
+                   control.fit,control.optim,groupfromy=NULL)
 {
-# unpack things:
+  # unpack things:
   hessian=control.fit$hessian
   nx=control.fit$nx
   theta.f=survey.pars$theta.f
@@ -1344,13 +1353,14 @@ fit.hmltm=function(xy,pars,FUN,models=list(y=NULL,x=NULL),survey.pars,hmm.pars,c
   Pi=hmm.pars$Pi
   delta=hmm.pars$delta
   
-# fit model
-  est=fit.xy(pars=pars,xy=xy,FUN=FUN,models=models,pm=pm,Pi=Pi,delta=delta,theta.f=theta.f,theta.b=theta.b,W=W,ymax=ymax,dy=dy,nx=nx,hessian=hessian,control=control.optim)
-
+  # fit model
+  est=fit.xy(pars=pars,xy=xy,FUN=FUN,models=models,pm=pm,Pi=Pi,delta=delta,theta.f=theta.f,theta.b=theta.b,W=W,ymax=ymax,dy=dy,nx=nx,hessian=hessian,
+             control=control.optim,groupfromy=groupfromy)
+  
   # store transformed parameters
   b=tfm(est$par,FUN)
   est$b=b
-
+  
   n=length(xy$x)
   xs=seq(0,W,length=nx)
   if(is.nullmodel(models)) {
@@ -1374,7 +1384,7 @@ fit.hmltm=function(xy,pars,FUN,models=list(y=NULL,x=NULL),survey.pars,hmm.pars,c
   llik=-est$value
   npar=length(est$par)
   aic=-2*llik+2*npar
-
+  
   # return it all:
   if(is.nullmodel(models)) pzero=px[1]
   else pzero=mean(px[,1])
@@ -1405,22 +1415,33 @@ fit.hmltm=function(xy,pars,FUN,models=list(y=NULL,x=NULL),survey.pars,hmm.pars,c
 #' @param ymax forward distance by which detection hazard has fallen to zero.
 #' @param dy Markov model distance step size.
 #' @param nx number of x-values to use in evaluating detection function.
-#' 
-negllik.xandy=function(b,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta,theta.f,theta.b,W,ymax,dy,nx=100)
+#' @param groupfromy a forward distance (y) below which all y's are grouped into a single
+#' interval in the likelihood function (i.e. exact y,s < groupfromy are combined into
+#' an interval rather than passed as exact distances).
+negllik.xandy=function(b,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta,theta.f,theta.b,W,ymax,dy,nx=100,groupfromy=NULL)
 {
-  covb=make.covb(b,FUN,models,xy) # put covariates into paramerters
-  x=xy$x;y=xy$y
+  # If asked to group close y's, split data accordingly:
+  if(!is.null(groupfromy)){
+    if(groupfromy<0) stop("groupfromy must be greater than zero.")
+    if(groupfromy>max(xy$y)) stop(paste("With groupfromy=",groupfromy,"you are grouping all forward distances: can't do this."))
+    grouped=xy$y<=groupfromy
+    xgrp=xy$x[grouped];ygrp=xy$y[grouped]
+    x=xy$x[!grouped];y=xy$y[!grouped]
+    covb=make.covb(b,FUN,models,xy[!grouped,]) # put covariates into paramerters; ungrouped data
+    covbgrp=make.covb(b,FUN,models,xy[grouped,]) # put covariates into paramerters; ungrouped data
+  } else {
+    x=xy$x;y=xy$y
+    covb=make.covb(b,FUN,models,xy) # put covariates into paramerters
+  }
+  
+  # Deal with the ungrouped data:
+  # ----------------------------
   n=length(x)
   if(length(y)!=n) stop("Length of y: ",length(y)," not equal to length of x: ",n,"\n")
   llik=0
   # calculate p(see first @ y|x) at each (x,y):
   li=p.xy(x,y,hfun=FUN,b=covb,pm=pm,Pi=Pi,delta=delta,ymax=ymax,dy=dy,theta.f=theta.f,theta.b=theta.b)
-  # calculate p(see by min y in window|x) over all x:
-  #*# xs=seq(0,W,length=nx)
-  #*# px=p.xy(x=xs,y=xy$y,hfun=FUN,b=b,pm=pm,Pi=Pi,delta=delta,ymax=ymax,dy=dy,theta.f=theta.f,theta.b=theta.b,ally=TRUE)
-  #*# wt=c(0.5,rep(1,(nx-2)),0.5)*W/nx
-  #*# p=sum(px*wt) # effective strip width
-  # cat("pars=",invtfm(b,FUN),"\n")
+  if(any(li<.Machine$double.xmin)) return(.Machine$double.xmax)
   xs=seq(0,W,length=nx)
   nb=length(covb)/n # number of parameters for each observation
   if(is.nullmodel(models)) {
@@ -1437,13 +1458,42 @@ negllik.xandy=function(b,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta,theta.f,t
     p=apply(ps,1,sintegral,xs)/W
   }
   
-  if(any(li<.Machine$double.xmin)) return(.Machine$double.xmax)
-  else{
-    llik=sum(log(li)-log(p))
-    return(-llik)
+  llik=sum(log(li)-log(p))
+  
+  
+  # Deal with the grouped data:
+  # ----------------------------
+  if(!is.null(groupfromy)){
+    ngrp=length(xgrp)
+    if(ngrp>0){
+      if(length(ygrp)!=ngrp) stop("Length of ygrp: ",length(ygrp)," not equal to length of xgrp: ",ngrp,"\n")
+      llik.grp=0
+      xs=seq(0,W,length=nx)
+      nb=length(covbgrp)/ngrp # number of parameters for each observation
+      if(is.nullmodel(models)) {
+        bi=c(rep(covbgrp[1:nb],nx)) # nx replicates of covbgrp for ith detection
+        ps=p.xy(x=xs,y=rep(0,nx),hfun=FUN,b=bi,pm=pm,Pi=Pi,delta=delta,ymax=ymax,dy=dy,theta.f=theta.f,theta.b=theta.b,ally=TRUE)
+        p=sintegral(ps,xs)/W
+      } else {
+        ps=matrix(rep(0,ngrp*nx),nrow=ngrp)
+        for(i in 1:ngrp) {
+          start=(i-1)*nb+1
+          bi=c(rep(covbgrp[start:(start+nb-1)],nx)) # nx replicates of covbgrp for ith detection
+          ps[i,]=p.xy(x=xs,y=rep(0,nx),hfun=FUN,b=bi,pm=pm,Pi=Pi,delta=delta,ymax=ymax,dy=dy,theta.f=theta.f,theta.b=theta.b,ally=TRUE)
+        }
+        p=apply(ps,1,sintegral,xs)/W
+      }
+      #  # calculate p(see first at or after groupfromy|x) at each x:
+      pfar=p.xy(x=xgrp,y=rep(groupfromy,ngrp),hfun=FUN,b=covbgrp,pm=pm,Pi=Pi,delta=delta,ymax=ymax,dy=dy,theta.f=theta.f,theta.b=theta.b,cdf=TRUE)
+      p0=p.xy(x=xgrp,y=rep(0,ngrp),hfun=FUN,b=covbgrp,pm=pm,Pi=Pi,delta=delta,ymax=ymax,dy=dy,theta.f=theta.f,theta.b=theta.b,cdf=TRUE)
+      pnear=p0-pfar
+      llik=llik+sum(log(pnear)-log(p))
+    }
   }
+  
   return(-llik)
 }
+
 
 #' @title HMM line transect model likelihood with only perpendicular distances.
 #'
@@ -1524,12 +1574,15 @@ negllik.x=function(b,xy,FUN,models,pm,Pi,delta,theta.f,theta.b,W,ymax,dy,nx=100)
 #' @param ymax forward distance by which detection hazard has fallen to zero.
 #' @param dy Markov model distance step size.
 #' @param nx number of x-values to use in evaluating detection function.
+#' @param groupfromy a forward distance (y) below which all y's are grouped into a single
+#' interval in the likelihood function (i.e. exact y,s < groupfromy are combined into
+#' an interval rather than passed as exact distances).
 #' 
-negllik.xy=function(b,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta,theta.f,theta.b,W,ymax,dy,nx=100)
+negllik.xy=function(b,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta,theta.f,theta.b,W,ymax,dy,nx=100,groupfromy=NULL)
 {
   xydat=xy[!is.na(xy$y),] # detections with x (perp) and y (forward) data
   xdat=xy[is.na(xy$y),]   # detections with x (perp) data only (no y data)
-  xy.negllik=negllik.xandy(b,xydat,FUN,models,pm,Pi,delta,theta.f,theta.b,W,ymax,dy,nx)
+  xy.negllik=negllik.xandy(b,xydat,FUN,models,pm,Pi,delta,theta.f,theta.b,W,ymax,dy,nx,groupfromy)
   x.negllik=0
   if(length(xdat$x)>0) negllik.x(b,xdat,FUN,models,pm,Pi,delta,theta.f,theta.b,W,ymax,dy,nx)
   negllik=xy.negllik+x.negllik
@@ -1555,9 +1608,11 @@ negllik.xy=function(b,xy,FUN,models=list(y=NULL,x=NULL),pm,Pi,delta,theta.f,thet
 #' Markov model governed by \code{Pi}.
 #' @param theta.f REDUNDANT must = 0.
 #' @param theta.b REDUNDANT must = 90.
-#' @param ally If TRUE calculates probability of detection at all - at some y. (Argument
+#' @param ally If TRUE calculates probability of detection at all (i.e., at some y. (Argument
 #' \code{cdf} is redundant in this case).
 #' @param cdf If TRUE returns 1 -(the cdf at y), which is equal to the probability of detection BY y.
+#' This differes from specifying ally=TRUE in that ally=TRUE calculates the cdf from ymax 
+#' to y=0, whereas cdf=TRUE calculates the cdf from ymax to y.
 #' 
 #' @seealso \code{\link{p.xy1}}
 #' 
@@ -2496,6 +2551,9 @@ NDest=function(dat,hmltm.fit,W){
 #' @param W.est right truncation perpendicular distance for estimation. Can't be less than maximum 
 #' perpendicular distance (x) in the line transect data frame \code{dat}, but can be less than 
 #' the max perpendicular distance used for fitting (\code{survey.pars$W}).
+#' @param groupfromy a forward distance (y) below which all y's are grouped into a single
+#' interval in the likelihood function (i.e. exact y,s < groupfromy are combined into
+#' an interval rather than passed as exact distances).
 #'
 #' @return A list with four elements: \code{hmltm.fit}, \code{point}, \code{dat}, \code{W.est}. 
 #' Their contents are as follows:
@@ -2538,19 +2596,19 @@ NDest=function(dat,hmltm.fit,W){
 est.hmltm=function(dat,
                    pars,FUN,models=list(y=NULL,x=NULL),
                    survey.pars,hmm.pars,control.fit,control.opt,
-                   twosit=FALSE,notrunc=FALSE,W.est=NULL){
+                   twosit=FALSE,notrunc=FALSE,W.est=NULL,groupfromy=NULL){
   # convert mrds data to cds data format (combining detections)
   if(twosit) dat1=make.onesit(dat) else dat1=dat
   # data truncation:
   if(!notrunc) if(min(na.omit(dat1$x)) < survey.pars$Wl | survey.pars$W < max(na.omit(dat1$x))) 
     dat1=truncdat(dat1,minval=survey.pars$Wl,maxval=survey.pars$W,twosit=FALSE)
   # extract sightings rows only
-#  sits1=!is.na(dat1$seen)
-#  srows1=sits1 & dat1$seen==1 # mark rows that have sightings
+  #  sits1=!is.na(dat1$seen)
+  #  srows1=sits1 & dat1$seen==1 # mark rows that have sightings
   srows1=!is.na(dat1$object)
   sdat1=dat1[srows1,]
   # fit the model:
-  hmltm.fit=fit.hmltm(sdat1,pars,FUN,models,survey.pars,hmm.pars,control.fit,control.opt)
+  hmltm.fit=fit.hmltm(sdat1,pars,FUN,models,survey.pars,hmm.pars,control.fit,control.opt,groupfromy=groupfromy)
   # estimate density, etc:
   if(is.null(W.est)) W.est=survey.pars$W
   if(!is.null(survey.pars$Wl)) W.est=survey.pars$W-survey.pars$Wl # since in this case data all shifted left by $Wl
