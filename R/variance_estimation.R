75028ca587e5cafb7f2814830273662e8d367f78
TheoMichelot <theo.michelot@gmail.com>
2015-11-03 16:25:25 +0000

    cosmetic changes in var estimation functions

--- R/variance_estimation.R
+++ R/variance_estimation.R
@@ -22,46 +22,60 @@
 #' \code{\link{splinefun}} for approximation of function derivatives.
 #' 
 #' @export
-invHessvar=function(stat,hmmlt,nx4spline=50,doplot=FALSE){
+invHessvar <- function(stat,hmmlt,nx4spline=50,doplot=FALSE)
+{
   # extract things we need:
-  models=hmmlt$models
-  if(!is.nullmodel(models)) stop("This function only coded for models without covariates.")
-  cov=hmmlt$xy[1,] # extract only 1 obs 'cause assuming no covars, so stats same for all obs
-  hfun=hmmlt$h.fun
-  b=hmmlt$fit$b
-  hmm.pars=hmmlt$fitpars$hmm.pars
-  survey.pars=hmmlt$fitpars$survey.pars
-  vcv=solve(hmmlt$fit$hessian)
-  vlen=dim(vcv)[1]
+  models <- hmmlt$models
+  
+  if(!is.nullmodel(models)) 
+    stop("This function only coded for models without covariates.")
+  
+  cov <- hmmlt$xy[1,] # extract only 1 obs 'cause assuming no covars, so stats same for all obs
+  hfun <- hmmlt$h.fun
+  b <- hmmlt$fit$b
+  hmm.pars <- hmmlt$fitpars$hmm.pars
+  survey.pars <- hmmlt$fitpars$survey.pars
+  vcv <- solve(hmmlt$fit$hessian)
+  vlen <- dim(vcv)[1]
+  
   # calculate derivative vector
-  dstat.db=rep(0,vlen)
-  no.se=FALSE
+  dstat.db <- rep(0,vlen)
+  no.se <- FALSE
+  
   for(i in 1:vlen){
-    se=sqrt(vcv[i,i])
+    se <- sqrt(vcv[i,i])
+    
     if(is.nan(se)) {
-      no.se=TRUE
-      x=seq(b[i]-b[i]/10,b[i]+b[i]/10,length=nx4spline)
-    } else {x=seq(b[i]-se/2,b[i]+se/2,length=nx4spline)}
-    y=x*0
+      no.se <- TRUE
+      x <- seq(b[i]-b[i]/10,b[i]+b[i]/10,length=nx4spline)
+    } else 
+      x <- seq(b[i]-se/2,b[i]+se/2,length=nx4spline)
+    
+    y <- x*0
+    
     for(j in 1:nx4spline) {
-      bi=b
-      bi[i]=x[j]
-      y[j]=hmltm.stat(stat,bi,hfun,models,cov,survey.pars,hmm.pars)
+      bi <- b
+      bi[i] <- x[j]
+      y[j] <- hmltm.stat(stat,bi,hfun,models,cov,survey.pars,hmm.pars)
     }
-    sf=splinefun(x,y)
+    
+    sf <- splinefun(x,y)
     if(doplot) {
       plot(x,y,type="l")
       lines(x,sf(x),col="red",lty=2)
     }
-    dstat.db[i]=sf(b[i],deriv=1)
+    
+    dstat.db[i] <- sf(b[i],deriv=1)
   }
+  
   # use inverse Hessian to calculate variance of statistic:
-  if(no.se) return(list(invHess=vcv,dstat.db=dstat.db,stat=stat,se=-999,cv=-999))
+  if(no.se) 
+    return(list(invHess=vcv,dstat.db=dstat.db,stat=stat,se=-999,cv=-999))
   else {
-    VAR=t(dstat.db%*%vcv%*%dstat.db)
-    SE=sqrt(VAR)
-    stat=hmltm.stat(stat,b,hfun,models,cov,survey.pars,hmm.pars) # called with original b parameters
-    CV=SE/stat
+    VAR <- t(dstat.db%*%vcv%*%dstat.db)
+    SE <- sqrt(VAR)
+    stat <- hmltm.stat(stat,b,hfun,models,cov,survey.pars,hmm.pars) # called with original b parameters
+    CV <- SE/stat
     return(list(invHess=vcv,dstat.db=dstat.db,stat=stat,se=SE,cv=CV))
   }
 }
