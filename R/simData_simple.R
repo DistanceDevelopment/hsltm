d1fa16bf750d871dc09397e26223b43e073a3a0c
TheoMichelot <theo.michelot@gmail.com>
2015-11-03 10:34:40 +0000

    simulate given nbAnimals instead of nbObs

--- R/simData_simple.R
+++ R/simData_simple.R
@@ -3,7 +3,7 @@
 #' 
 #' Simulate detections in a hidden state line transect model with one observer.
 #' 
-#' @param nbObs Number of observations to simulate
+#' @param nbAnimals Number of animals
 #' @param pcu Vector of state dependent availability parameters (Pr(available|state))
 #' @param gamma Transition probability matrix of the hidden state process
 #' @param b Vector of parameters of the detection hazard function
@@ -19,29 +19,29 @@
 #' gamma <- matrix(c(0.9,0.1,0.1,0.9),nrow=2)
 #' b <- c(0.9,0.75,10)
 #' hfun <- "h.EP2.0"
-#' data <- simData_simple(nbObs=100,pcu=pcu,gamma=gamma,b=b,hfun=hfun,xmax=80,ymax=80,ystep=1)
+#' data <- simData_simple(nbAnimals=100,pcu=pcu,gamma=gamma,b=b,hfun=hfun,xmax=80,ymax=80,ystep=1)
 #' 
 #' @export
 
-simData_simple <- function(nbObs,pcu,gamma,b,hfun,xmax,ymax,ystep)
+simData_simple <- function(nbAnimals,pcu,gamma,b,hfun,xmax,ymax,ystep)
 {
   # data frame of observations
-  data <- data.frame(x=rep(NA,nbObs),y=rep(NA,nbObs))
+  data <- data.frame(x=numeric(),y=numeric())
   
   # initial distribution of the Markov chain
   delta <- solve(t(diag(2)-gamma+1),rep(1,2))
   
-  obs <- 1 # observation index
+  zoo <- 1 # animal index
   
-  # while less than nbObs detections have occurred...
-  while(obs<=nbObs) {
+  # loop over the animals
+  while(zoo<=nbAnimals) {
     t <- 1 # time index
     S <- sample(1:2,size=1,prob=delta) # state process
     A <- sample(0:1,size=1,prob=c(1-pcu[S],pcu[S])) # availability process
     x <- runif(1,0,xmax)
-    ystart <- ymax-runif(1,0,ystep)
-    # ystart <- ymax
-    y <- seq(ystart,0,by=-ystep)
+    # ystart <- ymax+runif(1,0,ystep)
+    ystart <- ymax
+    y <- seq(ystart,0,by=-ystep) # grid of y values
     
     if(A==1 & runif(1)<h_rcpp(x,y[t],b,hfun))
       D <- TRUE # detection process
@@ -62,12 +62,14 @@ simData_simple <- function(nbObs,pcu,gamma,b,hfun,xmax,ymax,ystep)
     }
     
     # if detected
-    if(D) {
-      data[obs,] <- c(x,y[t])
-      obs <- obs+1 
-    }
+    if(D)
+      data <- rbind(data,c(x,y[t]))
+    
     # else, the animal has gone through the observed area without being detected
+    
+    zoo <- zoo+1 # increment animal index
   }
   
+  colnames(data) <- c("x","y")
   return(data)
 }
