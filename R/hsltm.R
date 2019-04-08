f7877926009f3263d45ba6b49d34fa26f637a3c7
david-borchers <dlb@st-andrews.ac.uk>
2019-04-08 20:16:32 +0100

    added ht to beaked whale data and updated documentation

--- R/hsltm.R
+++ R/hsltm.R
@@ -4142,6 +4142,7 @@ NULL
 #'    \item{\code{y}:}{ perpendicular distances to detections (NA if no detection).}
 #'    \item{\code{size}:}{ size of each detected group (NA if no detection).}
 #'    \item{\code{bf}:}{ Beaufort sea state at the time of each detection (NA if no detection).}
+#'    \item{\code{ht}:}{ Observation platform height in m (NA if no detection).}
 #'    \item{\code{object}:}{ unique identifier for each detection (a numeric vector; NA if no detection).}
 #'  }
 #' @details Test dataset that contains only detections (transects without detections have been omitted). 
